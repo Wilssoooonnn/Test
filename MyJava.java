6d377b215c4b48bb6a79e4381e7866fd91c11395
nalifc <adamnuralifi11@gmail.com>
2023-09-11 09:11:28 +0700

    HitungKelilingPersegi

--- MyJava.java
+++ MyJava.java
@@ -6,9 +6,14 @@ import java.util.Scanner;
 public class MyJava {
     public static void main(String[] args) {
         Scanner x = new Scanner(System.in);
-        System.out.print("Input :");
-        String teks = x.nextLine();
-        System.out.print("Input yang dimasukkan adalah :" +teks);
+        System.out.print("Input panjang : ");
+        float panjang = x.nextFloat();
+
+        System.out.print("Input lebar : ");
+        float lebar = x.nextFloat();
+
+        float keliling = 2 * (panjang+lebar);
+        System.out.print("Hasil : " +keliling);
         
     }
 }
